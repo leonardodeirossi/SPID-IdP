01aaf2693da79e3228c39dc46a2a19bb587b6317
Leonardo Essam <leonardoessam.deirossi@gmail.com>
2026-02-17 10:34:37 +0100

    Aggiunto autoload

---
+++ vendor/autoload.php
@@ -0,0 +1,4 @@
+<?php
+require_once(__DIR__ . "/oauth2-server-php-1.14.2/src/OAuth2/Autoloader.php");
+require_once(__DIR__ . "/Bramus/Router.php");
+require_once(__DIR__ . "/AgID/SPID/autoload.php");
